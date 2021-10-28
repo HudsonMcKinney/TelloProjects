545d7c5dd7dd1b175dcce3cdd8c867b88cca720c
Hudson McKinney <93225633+HudsonMcKinney@users.noreply.github.com>
2021-10-28 14:15:03 -0700

    Update BasicMovements.py

--- BasicMovements.py
+++ BasicMovements.py
@@ -1,14 +1,33 @@
 from djitellopy import Tello
+from time import sleep
 
 tello = Tello()
 
+tello.connect()
+
 tello.move_up(90)
+
+sleep(0.25)
+
 tello.move_forward(100)
+
+sleep(0.25)
+
 tello.move_right(100)
+
+sleep(0.25)
+
 tello.move_back(100)
+
+sleep(0.25)
+
 tello.move_left(100)
 
+sleep(0.25)
+
 tello.flip_forward()
+
+sleep(0.25)
 
 tello.land()
 tello.end()
