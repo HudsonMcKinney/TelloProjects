0407e176cf1def10ab4bd4115aaf8d39c7e69abb
Hudson McKinney <93225633+HudsonMcKinney@users.noreply.github.com>
2021-11-18 14:21:48 -0800

    removed potential for compounding errors

--- FlightPlan1.py
+++ FlightPlan1.py
@@ -5,42 +5,42 @@ tello = Tello()
 tello.connect()
 print(tello.get_battery())
 
-feet = 31
+feet = 30.48
 
 tello.takeoff()
 sleep(.5)
-tello.move_up(3*feet)
+tello.move_up(round(3*feet))
 sleep(.5)
-tello.move_forward(5*feet)
+tello.move_forward(round(5*feet))
 sleep(.5)
 tello.rotate_counter_clockwise(90)
 sleep(.5)
 # first straight
 
 
-tello.move_forward(6*feet)
+tello.move_forward(round(6*feet))
 sleep(.5)
 tello.rotate_clockwise(90)
 sleep(.5)
 # 2nd straight
 
-tello.move_down(3*feet)
+tello.move_down(round(3*feet))
 sleep(.5)
-tello.move_forward(3*feet)
+tello.move_forward(round(3*feet))
 sleep(.5)
 tello.rotate_clockwise(90)
 sleep(.5)
 # 3rd straight
 
-tello.move_up(1*feet)
+tello.move_up(round(1*feet))
 sleep(.5)
-tello.move_forward(3*feet)
+tello.move_forward(round(3*feet))
 sleep(.5)
 tello.rotate_counter_clockwise(90)
 sleep(.5)
 # 4th straight
 
-tello.move_forward(6*feet)
+tello.move_forward(round(6*feet))
 sleep(.5)
 tello.land()
 # 5th straight
