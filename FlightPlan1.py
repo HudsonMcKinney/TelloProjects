3ffeabfe3779e89f0387438c59ef4ebcccbd243e
Hudson McKinney <93225633+HudsonMcKinney@users.noreply.github.com>
2021-11-29 12:43:07 -0800

    Update FlightPlan1.py

--- FlightPlan1.py
+++ FlightPlan1.py
@@ -1,12 +1,15 @@
 from djitellopy import Tello
 from time import sleep
 
+# drone start sequence 
 tello = Tello()
 tello.connect()
 print(tello.get_battery())
 
+# converts centimeters into feet
 feet = 30.48
 
+# drone moves up, moves forward, then turns
 tello.takeoff()
 sleep(.5)
 tello.move_up(round(3.333*feet))
@@ -17,13 +20,14 @@ tello.rotate_counter_clockwise(90)
 sleep(.5)
 # first straight
 
-
+# drone moves forward and turns
 tello.move_forward(round(6*feet))
 sleep(.5)
 tello.rotate_clockwise(90)
 sleep(.5)
 # 2nd straight
 
+# drone moves down, then forward, then turns
 tello.move_down(round(3*feet))
 sleep(.5)
 tello.move_forward(round(3*feet))
@@ -32,6 +36,7 @@ tello.rotate_clockwise(90)
 sleep(.5)
 # 3rd straight
 
+# Drone moves up, then moves forward, then turns
 tello.move_up(round(1*feet))
 sleep(.5)
 tello.move_forward(round(3*feet))
@@ -40,6 +45,7 @@ tello.rotate_counter_clockwise(90)
 sleep(.5)
 # 4th straight
 
+# drone moves forward and lands
 tello.move_forward(round(6*feet))
 sleep(.5)
 tello.land()
